4a0f8eee75981ff0284b9ad9d41e5bb88b21d9bf
GiangHoGoVap <58210406+GiangHoGoVap@users.noreply.github.com>
2022-06-08 22:33:25 +0700

    Update TestUtils.py and CodeGenSuite.py

--- Assignment/assignment4/src/test/CodeGenSuite.py
+++ Assignment/assignment4/src/test/CodeGenSuite.py
@@ -4,14 +4,11 @@ from AST import *
 
 
 class CheckCodeGenSuite(unittest.TestCase):
-    def test_int(self):
-        """Simple program: int main() {} """
-        input = """void main() {putInt(100);}"""
-        expect = "100"
-        self.assertTrue(TestCodeGen.test(input,expect,500))
-    def test_int_ast(self):
-    	input = Program([
-    		FuncDecl(Id("main"),[],VoidType(),Block([],[
-    			CallExpr(Id("putInt"),[IntLiteral(5)])]))])
-    	expect = "5"
-    	self.assertTrue(TestCodeGen.test(input,expect,501))
+    def test_simple_program(self):
+        input = """Class Program{
+            main(){
+                io::$writeInt(1);
+            }
+        }"""
+        expect = "1"
+        self.assertTrue(TestCodeGen.test(input, expect, 500))
